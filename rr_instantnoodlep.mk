f2a6572b3cdfc18e4faf3632753b5d0a1eed0ed6
Varun Date <date.varun123@gmail.com>
2020-09-26 21:04:22 +1000

    RRify
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- rr_instantnoodlep.mk
+++ rr_instantnoodlep.mk
@@ -22,9 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_instantnoodlep
+# Inherit PixelGApps
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
+PRODUCT_NAME := rr_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -32,9 +35,16 @@ PRODUCT_MODEL := ONEPLUS IN2023
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus8Pro \
     PRODUCT_NAME=OnePlus8Pro \
     PRIVATE_BUILD_DESC="OnePlus8Pro-user 10 QKQ1.191222.002 2005301332 release-keys"
 
 BUILD_FINGERPRINT := OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2005301332:user/release-keys
+
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RR_BUILDTYPE := Official
